2ea236dde8b55fccbf8de1c353f6239bbf82e6a4
Tyler Arciniaga <tylerarc@umich.edu>
2026-02-21 19:01:25 -0500

    chore: read file contents and sha256 hash them as checksum

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
+	"io"
 	"io/fs"
+	"os"
 	"path/filepath"
 )
 
@@ -15,15 +18,48 @@ func ingestDirectory(path string, d fs.DirEntry, err error) error {
 	if err != nil {
 		return err
 	}
+
+	fmt.Println("Filepath:", path)
+	fmt.Println("Is Directory:", d.IsDir())
+
 	if !d.IsDir() {
+		//DirEntry is a file...
 		ext := filepath.Ext(filepath.Base(path))
 		if ext != ".txt" && ext != ".md" {
 			return nil
 		} //skip files that are not txt or md
+
+		err = readFile(path)
+		if err != nil {
+			return err
+		}
 	}
 
-	fmt.Println("Filepath:", path)
-	fmt.Println("File:", filepath.Base(path))
-	fmt.Println("Is Directory:", d.IsDir())
+	return nil
+}
+
+func readFile(filepath string) error {
+	fmt.Println("File:", filepath)
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+
+	var checksum [32]byte
+	buf := make([]byte, 4096)
+	for {
+		n, err := file.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+
+		checksum = sha256.Sum256(buf[:n])
+		fmt.Println("Checksum:", checksum)
+	}
+
 	return nil
 }
